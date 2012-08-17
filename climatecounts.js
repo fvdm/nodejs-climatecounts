83aa97e6fec0ce18dea18447fff6bab498a808ec
Franklin van de Meent <fr@nkl.in>
2012-08-17 04:04:51 +0300

    Simplified talk()
    The API only accepts GET request, therefore http.get makes the process much simpler.

--- climatecounts.js
+++ climatecounts.js
@@ -34,13 +34,6 @@ var http = require('http')
 
 var app = {
   
-  // API access details
-  api: {
-    host:    'api.climatecounts.org',
-    port:    80,
-    version: 1
-  },
-  
   // Available years
   availableyears: function( cb ) {
     app.talk( 'AvailableYears', cb )
@@ -103,35 +96,18 @@ var app = {
       var fields = {}
     }
     
-    var req = http.request(
-      {
-        host:   app.api.host,
-        port:   app.api.port,
-        path:   '/'+ app.api.version +'/'+ path +'.json',
-        method: 'GET'
-      },
-      function( response ) {
-        var data = ''
-        response.on( 'data', function( part ) { data += part })
-        response.on( 'end', function() {
-          data = data.toString().trim()
-          
-          if( data.length >= 2 && data.substr(0,1) == '{' && data.substr( data.length -2, 1 ) == '}' ) {
-            cb( JSON.parse( data ) )
-          } else {
-            cb( false )
-          }
-          
-          req.end()
-        })
-        response.on( 'close', function() {
-          req.end()
-        })
-      }
-    )
-    
-    req.on( 'error', function() {
-      req.end()
+    var url = 'http://api.climatecounts.org/1/'+ path +'.json'
+    http.get( url, function( response ) {
+      var data = ''
+      response.on( 'data', function( part ) { data += part })
+      response.on( 'end', function() {
+        data = data.toString().trim()
+        if( data.length >= 2 && data.substr(0,1) == '{' && data.substr( data.length -2, 1 ) == '}' ) {
+          cb( JSON.parse( data ) )
+        } else {
+          cb( false )
+        }
+      })
     })
   }
 }
